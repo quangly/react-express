d6dcc4665a180e4348ff49299d3b0a7173cceca0
Quang Ly <quangdly@gmail.com>
2017-04-10 16:17:32 -0400

    add/delete items

---
+++ app/actions/GroceryItemActionCreator.jsx
@@ -0,0 +1,16 @@
+var dispatcher = require('./../dispatcher.js');
+
+module.exports = {
+    add:function(item){
+        dispatcher.dispatch({
+            payload:item,
+            type:"grocery-item:add"
+        })
+    },
+    delete:function(item){
+        dispatcher.dispatch({
+            payload:item,
+            type:"grocery-item:delete"
+        })
+    }    
+}
